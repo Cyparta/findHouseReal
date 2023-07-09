dedb2948ab3aaaec2ff790bf268c9d8fb59723a7
Reem99-sina <reemsina2@gmail.com>
2023-07-09 11:30:42 +0300

    fresh id

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,14 +7,7 @@ import "../index.scss";
 
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
-  ( function(l){
-      if(l.search[1]==="/"){
-        var decoded=l.search.slice(1).split("&").map((s)=>{
-          return s.replace(/~and~/g,'&')
-        }).join('?')
-        window.history.replaceState(null,null,l.pathname.slice(0,-1)+decoded+l.hash)
-      }
-    }(window?.location))
+
 }
 
 function MyApp({ Component, pageProps }) {
