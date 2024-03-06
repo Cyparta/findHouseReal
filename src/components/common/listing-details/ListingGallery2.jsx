b127ba6274e725f952215e5d5e9a67b5e4bd3cfe
khaledgammal7 <khaledgammal7@gmail.com>
2024-03-06 18:04:50 +0200

    a

--- src/components/common/listing-details/ListingGallery2.jsx
+++ src/components/common/listing-details/ListingGallery2.jsx
@@ -3,110 +3,121 @@ import "photoswipe/dist/photoswipe.css";
 import propertiesContent from "../../../data/properties";
 import { useSelector } from "react-redux";
 
-const ListingGallery2 = () => {
-  const lang = useSelector(state => state.lang.value.lang)
+const ListingGallery = () => {
+  const lang = useSelector((state) => state.lang.value.lang);
   return (
     <>
       <Gallery>
-        {propertiesContent.slice(37, 38).map((singleItem) => (
-          <div className="row g-0" key={singleItem.id[lang]}>
-            <div className="col-md-6 col-lg-6 ">
-              <div className="row g-0">
-                <div className="col-lg-12 ">
-                  <div className="spls_style_one pr1 1px position-relative">
-                    <Item
-                      original={singleItem.img[lang]}
-                      thumbnail={singleItem.img[lang]}
-                      width={752}
-                      height={450}
-                    >
-                      {({ ref, open }) => (
-                        <>
-                          <div className="single_property_title position-static">
-                            <div
-                              className="upload_btn popup-img"
-                              ref={ref}
-                              onClick={open}
-                            >
-                              <span className="flaticon-photo-camera"></span>{" "}
-                              View Photos
-                            </div>
-                          </div>
-                          <img
-                            className="img-fluid w100 lds-2"
-                            src={singleItem.img[lang]}
-                            alt={singleItem.img[lang]}
-                          />
-                        </>
-                      )}
-                    </Item>
+        {propertiesContent.slice(0, 3).map((singleItem) => (
+          <div key={singleItem.id}>
+            <div className="row mb30">
+              <div className="col-lg-7 col-xl-8">
+                <div className="single_property_title mt30-767">
+                  <h2>{singleItem.title[lang]}</h2>
+                  <p>{singleItem.location[lang]}</p>
+                </div>
+              </div>
+              <div className="col-lg-5 col-xl-4">
+                <div className="single_property_social_share position-static transform-none">
+                  <div className="price float-start fn-400">
+                    <h2>
+                      ${singleItem.price[lang]}
+                      <small>/mo</small>
+                    </h2>
                   </div>
+
+                  <div className="spss style2 mt20 text-end tal-400">
+                    <ul className="mb0">
+                      <li className="list-inline-item">
+                        <a href="#">
+                          <span className="flaticon-transfer-1"></span>
+                        </a>
+                      </li>
+                      <li className="list-inline-item">
+                        <a href="#">
+                          <span className="flaticon-heart"></span>
+                        </a>
+                      </li>
+                      <li className="list-inline-item">
+                        <a href="#">
+                          <span className="flaticon-share"></span>
+                        </a>
+                      </li>
+                      <li className="list-inline-item">
+                        <a href="#">
+                          <span className="flaticon-printer"></span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  {/* End activity and social sharing */}
                 </div>
               </div>
             </div>
-            {/* End .col */}
+            {/* End .row */}
 
-            <div className="col-md-6 col-lg-6 position-relative">
-              <div className="row g-0 gx-0">
-                {singleItem.imgList2.map((val, i) => (
-                  <div className="col-6 col-lg-6 " key={i}>
-                    <div className="spls_style_one">
+            <div className="row">
+              <div className="col-sm-7 col-lg-8">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="spls_style_two mb30-520">
                       <Item
-                        original={val[lang]}
-                        thumbnail={val[lang]}
+                        original={singleItem.img[lang]}
+                        thumbnail={singleItem.img[lang]}
                         width={752}
                         height={450}
                       >
                         {({ ref, open }) => (
                           <div role="button" ref={ref} onClick={open}>
                             <img
-                              className="img-fluid w100"
-                              src={val}
-                              alt={val}
+                              className="img-fluid w100 lds-1"
+                              src={singleItem.img[lang]}
+                              alt="1.jpg"
                             />
                           </div>
                         )}
                       </Item>
                     </div>
                   </div>
-                ))}
-              </div>
-              {/* End .row */}
-
-              <div className="single_property_social_share">
-                <div className="spss style2 mt10 text-right tal-400">
-                  <ul className="mb0">
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-transfer-1"></span>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-heart"></span>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-share"></span>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-printer"></span>
-                      </a>
-                    </li>
-                  </ul>
                 </div>
               </div>
-              {/* End .single_property_social_share */}
+              {/* End .col-sm-7 .col-lg-8 */}
+
+              <div className="col-sm-5 col-lg-4">
+                <div className="row">
+                  {singleItem.imgList.map((val, i) => (
+                    <div className="col-6" key={i}>
+                      <div className="spls_style_two img-gallery-box mb24">
+                        <Item
+                          original={val}
+                          thumbnail={val}
+                          width={752}
+                          height={450}
+                        >
+                          {({ ref, open }) => (
+                            <div role="button" ref={ref} onClick={open}>
+                              <img
+                                className="img-fluid w100"
+                                src={val}
+                                alt="2.jpg"
+                              />
+                            </div>
+                          )}
+                        </Item>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              {/* End  col-sm-5 col-lg-4 */}
             </div>
-            {/* End .col */}
+            {/* End .row */}
           </div>
+          // End single item loop
         ))}
       </Gallery>
     </>
   );
 };
 
-export default ListingGallery2;
+export default ListingGallery;
--- src/pages/units-details/[id].jsx
+++ src/pages/units-details/[id].jsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import Seo from '../../components/common/seo'
-import MobileMenu from '../../components/common/header/MobileMenu'
-import PopupSignInUp from '../../components/common/PopupSignInUp'
-import Footer from '../../components/common/footer/Footer'
-import CopyrightFooter from '../../components/common/footer/CopyrightFooter'
-import { useRouter } from 'next/router'
-import axios from 'axios'
-import Header from '../../components/common/header/DefaultHeader'
-import { useSelector } from 'react-redux'
-import styles from './styles.module.css'
+import React, { useEffect, useState } from "react";
+import Seo from "../../components/common/seo";
+import MobileMenu from "../../components/common/header/MobileMenu";
+import PopupSignInUp from "../../components/common/PopupSignInUp";
+import Footer from "../../components/common/footer/Footer";
+import CopyrightFooter from "../../components/common/footer/CopyrightFooter";
+import { useRouter } from "next/router";
+import axios from "axios";
+import Header from "../../components/common/header/DefaultHeader";
+import { useSelector } from "react-redux";
+import styles from "./styles.module.css";
 
+import "photoswipe/dist/photoswipe.css";
+
+import { Gallery, Item } from "react-photoswipe-gallery";
 
 // export async function getServerSideProps(context) {
 //     const productId = context.params?.id;
@@ -34,97 +37,153 @@ import styles from './styles.module.css'
 // }
 
 const UnitsDetails = () => {
-    const lang = useSelector((state) => state.lang.value.lang);
-    const router = useRouter();
-    console.log(router.query.id)
-    const [data, setData] = useState([])
+  const lang = useSelector((state) => state.lang.value.lang);
+  const router = useRouter();
+  console.log(router.query.id);
+  const [data, setData] = useState([]);
 
-    console.log(data)
+  console.log(data);
 
-    const fetchDet = async () => {
-        try {
-            const res = await axios.get(`https://dolphin-app-u2qj5.ondigitalocean.app/en/aqar/api/router/website-units/${router?.query?.id}/`)
-            setData(res.data)
-        } catch (err) {
-            console.log(err)
-        }
+  const fetchDet = async () => {
+    try {
+      const res = await axios.get(
+        `https://dolphin-app-u2qj5.ondigitalocean.app/en/aqar/api/router/website-units/${router?.query?.id}/`
+      );
+      setData(res.data);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    useEffect(() => {
-        if (router?.query?.id !== undefined) {
-            fetchDet()
-        }
-    }, [router?.query?.id])
-    return (
-        <>
-            <div className={lang}>
-                <Seo pageTitle="Home-1" />
-                <Header />
-                <MobileMenu />
-                <PopupSignInUp />
-                  
+  console.log(data);
 
-                
+  useEffect(() => {
+    if (router?.query?.id !== undefined) {
+      fetchDet();
+    }
+  }, [router?.query?.id]);
+  return (
+    <>
+      <div className={lang}>
+        <Seo pageTitle="Home-1" />
+        <Header />
+        <MobileMenu />
+        <PopupSignInUp />
 
-
-
-
-
-
-
-
-
-
-
-                <section className='mt85'>
-                    <div className="container">
-                        <div className="row align-items-between">
-                            <div className="col-md-6 ">
-                                <img src={data?.main_image} className=' imagesize' alt={data?.address} style={{ borderRadius: '18px' }} />
-                            </div>
-                            <div className="col-md-6  boxdetails">
-                                <p className={styles.lead_heading}>
-                                    {data?.address}
-                                </p>
-                                <p className={styles.lead_unit}>
-                                    {data?.city}
-                                </p>
-
-                                <div className={`d-flex gap-2`} >
-                                    <p className={styles.lead_unit}>غرف النوم : {data?.kitchen_count}</p>
-                                    <p className={styles.lead_unit}>مطبخ : {data?.kitchen_count}</p>
-                                </div>
-
-                                <p className={styles.lead_unit}>{data?.complete_type}</p>
-                                <p className={styles.lead_unit}>{data?.country}</p>
-                                <p className={styles.lead_unit}>{data?.unit_type}</p>
-                                <p className={styles.lead_unit}>{data?.state}</p>
-                                <p className={styles.lead_unit}>السعر : {data?.price}</p>
-                                <p className={styles.lead_unit}>{data?.comment}</p>
-                                <a href='tel:{data?.phone}' className={styles.lead_unit}>للتواصل : {data?.phone}</a>
-                            </div>
-                        </div>
+        <section className="listing-title-area mt85">
+          <div className="container">
+            <div className="row mb30">
+              <div className="col-lg-7 col-xl-8">
+                <div className="single_property_title mt30-767">
+                  <p className={styles.lead_heading}>{data?.address}</p>
+                  <p className={styles.lead_unit}>{data?.city}</p>
+                </div>
+              </div>
+              <div className="col-lg-5 col-xl-4">
+                <div className="single_property_social_share position-static transform-none">
+                  <div className="price float-start fn-400">
+                    <div>
+                      <p className={styles.lead_unit}>
+                        غرف النوم : {data?.kitchen_count}
+                      </p>
+                      <p className={styles.lead_unit}>
+                        مطبخ : {data?.kitchen_count}
+                      </p>
+                      <p className={styles.lead_unit}>{data?.complete_type}</p>
+                      <p className={styles.lead_unit}>{data?.country}</p>
+                      <p className={styles.lead_unit}>{data?.unit_type}</p>
+                      <p className={styles.lead_unit}>{data?.state}</p>
+                      <p className={styles.lead_unit}>السعر : {data?.price}</p>
+                      <p className={styles.lead_unit}>{data?.comment}</p>
+                      <a href="tel:{data?.phone}" className={styles.lead_unit}>
+                        للتواصل : {data?.phone}
+                      </a>
                     </div>
-                </section>
+                  </div>
 
-                {/* <!-- Our Footer --> */}
-                <section className="footer_one">
-                    <div className="container">
-                        <div className="row">
-                            <Footer />
-                        </div>
-                    </div>
-                </section>
-
-                {/* <!-- Our Footer Bottom Area --> */}
-                <section className="footer_middle_area pt40 pb40">
-                    <div className="container">
-                        <CopyrightFooter />
-                    </div>
-                </section>
+                  {/* End activity and social sharing */}
+                </div>
+              </div>
             </div>
-        </>
-    )
-}
+            ;
+            <div className="row">
+              <div className="col-sm-7 col-lg-8">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="spls_style_two mb30-520">
+                      <div>
 
-export default UnitsDetails
+
+                        <Gallery>
+                          <Item
+                            original={data?.main_image}
+                            thumbnail={data?.main_image}
+                            width="1024"
+                            height="768"
+                          >
+                            {({ ref, open }) => (
+                              <img
+                                ref={ref}
+                                onClick={open}
+                                src={data?.main_image}
+                              />
+                            )}
+                          </Item>
+                          
+                        </Gallery>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="spss style2 mt20 text-end tal-400">
+                <ul className="mb0">
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <i className="flaticon-transfer-1"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <i className="flaticon-heart"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <i className="flaticon-share"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <i className="flaticon-printer"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            ;
+          </div>
+        </section>
+
+        {/* <!-- Our Footer --> */}
+        <section className="footer_one">
+          <div className="container">
+            <div className="row">
+              <Footer />
+            </div>
+          </div>
+        </section>
+
+        {/* <!-- Our Footer Bottom Area --> */}
+        <section className="footer_middle_area pt40 pb40">
+          <div className="container">
+            <CopyrightFooter />
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default UnitsDetails;
